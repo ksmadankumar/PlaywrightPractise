730db397bd7ffffcbdb92fd6b81197a2586e6120
ksmadankumar <ksmadankumar@gmail.com>
2026-07-03 21:06:02 +0530

    Javascript Problems

---
+++ Problems/reverse-string.js
@@ -0,0 +1,7 @@
+// How to reverse a string "Micro1"
+
+function reverseString(str){
+    return str.split("").reverse().join("");
+
+}
+console.log(reverseString("Micro1"))
---
+++ tests/first-test.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from '@playwright/test';
+
+test ("TC-001 Verify login is successful", async({page}) => {
+    await page.goto("https://brewhaus-store.vercel.app/login");
+    await page.getByTestId('username-input').fill("demo_user");
+    await page.getByTestId('password-input').fill("Brew@2026");
+    await page.getByTestId("login-button").click;
+
+    await expect(page).toHaveURL(/products/);
+    
+})
